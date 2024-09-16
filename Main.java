a996dea8bd4b63307b817e0ddb617565205ef140
iermina <167538000+iermina@users.noreply.github.com>
2024-09-16 22:42:47 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.print("Hello! My name is Ermina Itri");
+        System.out.println("I love to hang out with my friends after school");
+        System.out.print("-- Ermina");
+    }
+}
